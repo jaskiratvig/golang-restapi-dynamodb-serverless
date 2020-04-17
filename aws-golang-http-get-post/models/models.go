a2e72abc83470dd54bdf08598d616f9ed2ffff65
Jaskirat Vig <jvig2@illinois.edu>
2020-04-17 14:32:52 -0500

    auth0 in progress

--- aws-golang-http-get-post/models/models.go
+++ aws-golang-http-get-post/models/models.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"context"
+
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
 // Artist is our self-made struct to process JSON request from Client
 type Artist struct {
 	ArtistID    string   `json:"ArtistID"`
@@ -7,6 +14,20 @@ type Artist struct {
 	Songs       []string `json:"Songs"`
 	Subcategory string   `json:"Subcategory"`
 	Domestic    bool     `json:"Domestic"`
+}
+
+//Authenticator is our struct to create a new authenticator for Auth0
+type Authenticator struct {
+	Provider *oidc.Provider
+	Config   oauth2.Config
+	Ctx      context.Context
+}
+
+// SessionData is our struct for saving the state of our authentication request
+type SessionData struct {
+	ClientID string                 `json:"ClientID"`
+	State    string                 `json:"State"`
+	Profile  map[string]interface{} `json:"Profile"`
 }
 
 func (art Artist) editArtist(artist Artist) Artist {
